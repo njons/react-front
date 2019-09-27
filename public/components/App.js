20c923b6a31e5d2648d8db70b1d89c331d64677a
Nathalie Jonsson <njons@sunet.se>
2019-09-27 15:55:40 +0200

    adds fonts and styles to App.js

--- public/components/App.js
+++ public/components/App.js
@@ -3,7 +3,9 @@ class App extends React.Component {
   render() {
     return (
       <div className="container">
+
         <h1>This is App.js</h1>
+        <p>This a paragraph </p>
       </div>
     );
   }
