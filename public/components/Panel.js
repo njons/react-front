85310c6f0bc05e858bcf7099b6058aec490c8a08
Nathalie Jonsson <njons@sunet.se>
2019-10-18 12:37:53 +0200

    adds API request to App parent component, passes functionality from App parent to Panel child to trigger on button click

--- public/components/Panel.js
+++ public/components/Panel.js
@@ -1,6 +1,7 @@
 import React from "react";
 
 class Panel extends React.Component {
+  // the <button> comes with an onClick event that I have set to the requestProp functionality
   render() {
     return (
       <div id="panel">
@@ -8,11 +9,13 @@ class Panel extends React.Component {
 
         <div id="request">
           {/* <h2> Make a request </h2> */}
-          <button> Make a request </button>
+          <button onClick={this.props.requestData}> API request </button>
         </div>
         <div id="response">
           <h2> Get the response</h2>
-          <div id="data"></div>
+          <div id="data">
+          {/* data goes here */}
+          </div>
         </div>
       </div>
     );
